c6df934ebbf264cd0a270e1140d2ff4ae49dc5a5
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2024-03-24 13:45:24 +0500

    Update knexConfig.js

--- knexConfig.js
+++ knexConfig.js
@@ -2,10 +2,5 @@ const knex = require('knex')
 
 module.exports = knex({
     client: 'mysql', 
-    connection: {
-        host : 'bzxbwi6a8c677t1mhmhl-mysql.services.clever-cloud.com',
-        user : 'uuybf34djwzttmnq',
-        password : 'uuybf34djwzttmnq',
-        database : 'bzxbwi6a8c677t1mhmhl'
-    }
+    connection: 'mysql://uuybf34djwzttmnq:stMVUKK0bkH8A8BoiC7l@bzxbwi6a8c677t1mhmhl-mysql.services.clever-cloud.com:3306/bzxbwi6a8c677t1mhmhl'
 });
