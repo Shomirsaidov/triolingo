9b119bef10d2c6211bcc705f95a125bfa4511754
unknown <shomirsaidov.abubakr@mail.ru>
2024-03-27 17:40:27 +0500

    progress service - no bearer

--- knexConfig.js
+++ knexConfig.js
@@ -3,4 +3,5 @@ const knex = require('knex')
 module.exports = knex({
     client: 'mysql', 
     connection: 'mysql://uuybf34djwzttmnq:stMVUKK0bkH8A8BoiC7l@bzxbwi6a8c677t1mhmhl-mysql.services.clever-cloud.com:3306/bzxbwi6a8c677t1mhmhl'
+    // connection: 'mysql://'
 });
--- progress/handler.js
+++ progress/handler.js
@@ -6,6 +6,19 @@ const progressRoute = ['A1','A2','B1']
 
 
 module.exports = async (request, h) => {
+
+    let username_decoded;
+    try {
+        let verified = JWT.verify(request.query.token, 'mysecretKey')
+        let payload = JWT.decode(request.query.token)
+        username_decoded = payload.user
+    }
+    catch(e) {
+        console.log(`Error occured : ${e}`)
+        return {"statusCode":401,"error":"Unauthorized","message":"Missing authentication"}
+    }
+
+
     console.log("reported lesson completion !");
     console.log(request.url.pathname)
 
@@ -13,7 +26,7 @@ module.exports = async (request, h) => {
     let lesson_id = a[a.length - 1]
     
     try {   
-        let current_level = await knexInstance('users').select('level','lesson').where('name', request.auth.credentials.user)
+        let current_level = await knexInstance('users').select('level','lesson').where('name', username_decoded)
         let progress = `${current_level[0].level}-${current_level[0].lesson}`
         let new_level;
         if(progress == lesson_id) {
